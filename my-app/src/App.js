cf5b79a45f4edbea001290d012a700079485e5e1
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-10-13 18:33:40 -0700

    init: base code to start lynda mock site

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import APIContainer from './Components/API';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <APIContainer />
+      </div>
+    );
+  }
+}
+
+export default App;

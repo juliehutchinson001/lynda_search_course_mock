f4159e768a433feb93d44c9b561dde9a1d42358a
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-10-14 17:03:38 -0700

    feat: update components of lynda-mock site

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import Header from './Components/Header';
 import APIContainer from './Components/API';
+import Footer from './Components/Footer';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state={
+      search: ''
+    }
+
+  }
+
+  handleSearchOnChange(event) {
+    this.setState({search: event.target.value})
+  }
+
   render() {
     return (
       <div className="App">
+        <Header 
+          value={this.state.search}
+          onChange={ (event) => this.handleSearchOnChange(event) }
+        />
         <APIContainer />
+        <Footer />
       </div>
     );
   }

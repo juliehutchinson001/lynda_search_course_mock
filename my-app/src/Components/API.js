227d219d86733a85afb4419723fa75e85a87804d
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-10-18 20:50:14 -0700

    feat: implement lifecycle to receive data from lynda API

--- my-app/src/Components/API.js
+++ my-app/src/Components/API.js
@@ -10,6 +10,18 @@ class APIContainer extends Component {
         }
     }
 
+
+
+    componentDidMount() {
+
+        const query = `search?categoryName=javascript`;
+        const url = `https://mock-course-backend.herokuapp.com/${query}`;
+        
+        fetch(url)
+            .then(resp => resp.json())
+            .then(courses => this.setState({ courses: courses.course }))
+    }
+
     render() {
         const courses = this.state.courses.map(this.createCourse);
         return <section>{ courses }</section>;

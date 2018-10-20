07ab683be7703e2983ca9e3f39d6b43206ac52c5
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-10-20 14:17:04 -0700

    feat: layout/body of header component

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -15,6 +15,34 @@ class HeaderContainer extends Component {
     }
 }
 
+const Header = ({ value, handleSearch }) => (
 
+    <header>
+        <h1>Lynda Learning</h1>
+        
+        <p>
+            Welcome to the next stepping stone of your learning.
+            Type In your selection from the following options:
+        
+            <ul>
+                <li>Java</li>
+                <li>Javascript</li>
+                <li>Python</li>
+                <li>React</li>
+            </ul>
+        </p>
+        <input 
+            name='search' 
+            onChange={ handleSearch }
+            placeholder="search for a topic" 
+            type='search' 
+            value={ value } 
+        />
+        <button type='button' >Search</button>
+        <button type='button' >Show All</button>
+
+    </header>
+
+);
 
 export default HeaderContainer;

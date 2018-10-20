62fb77c8d46be5f7491ba80e3893354f25885eb2
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-10-20 13:54:00 -0700

    feat: implement query in url according to user selection

--- my-app/src/Helpers/get_query.js
+++ my-app/src/Helpers/get_query.js
@@ -2,7 +2,15 @@ const getQuery = searchedWord => {
 
     const categories = ["java", "javascript", "python", "react"];
 
-    
+    const queries = categories.filter(category => 
+        category === searchedWord.toLowerCase()
+    );
+
+    const invalidAnswer = "Search a valid term";
+    const verifySearch = queries.length ? queries[0] : invalidAnswer;
+
+
+    return verifySearch;
 
 };
 

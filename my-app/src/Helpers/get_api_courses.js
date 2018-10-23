73b50e4db1dffdf2cec2fb4c8a8953e31f421feb
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-10-23 16:55:55 -0700

    feat: final code to request courses through the url

--- my-app/src/Helpers/get_api_courses.js
+++ my-app/src/Helpers/get_api_courses.js
@@ -1,6 +1,11 @@
 const fetchCourses = (category, callback) => {
 
-    
+    const query = `search?categoryName=${category}`;
+    const url = `https://mock-course-backend.herokuapp.com/${query}`;
+
+    fetch(url)
+        .then(resp => resp.json())
+        .then(coursesObj => callback(coursesObj.course))
 
 };
 

ee1bad1c2ea7dadc40aff31f7a0d3f07ea2f6ff4
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-10-24 14:24:40 -0700

    style: update classNames and structure for BEM styling purposes

--- my-app/src/Components/Playlist.js
+++ my-app/src/Components/Playlist.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-const Playlist = () => (
+const Playlists = () => (
 
-    <aside>
-        <h1>Playlist</h1>
+    <aside id="#playlists" className="playlist" >
+        <h1 className="playlist playlist__header" >Playlists</h1>
+        <a className="playlist playlist__link" href="#playlists" > Show All </a>
+        <section className="playlist playlist__list" >
+            <nav>Here goes each one of my playlists</nav>        
+        </section>
     </aside>
 
 );
 
-export default Playlist;
+export default Playlists;

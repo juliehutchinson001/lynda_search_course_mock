296eca52b0af85b9f04149c79a68bbcb5acf84d9
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-10-24 14:36:44 -0700

    refactor: update structure and some classNames for styling purposes

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -7,11 +7,11 @@ class HeaderContainer extends Component {
 
         return (
             <Header 
-                value={ this.value } 
-                handleClickSearch={ this.handleClickSearch }   
-                handleSearch={ this.handleSearch }   
-                handleShowAll={ this.handleShowAll }   
-                handleEnterKeyPress={ this.handleEnterKeyPress }             
+                value={ this.props.value } 
+                handleClickSearch={ this.props.handleClickSearch }   
+                handleSearch={ this.props.handleSearch }   
+                handleShowAll={ this.props.handleShowAll }   
+                handleEnterKeyPress={ this.props.handleEnterKeyPress }             
             />
         );
 
@@ -20,31 +20,46 @@ class HeaderContainer extends Component {
 
 const Header = ({ value, handleClickSearch, handleSearch, handleEnterKeyPress, handleShowAll }) => (
 
-    <header>
-        <h1>Lynda Learning</h1>
-        
-        <p>
-            Welcome to the next stepping stone of your learning.
-            Type In your selection from the following options:
-        
-            <ul>
+    <header className="header" >
+        <article className="header header__intro" >
+            <h1 className="header header__title" >Lynda Learning</h1>
+            
+            <p className="header header__description" >
+                Welcome to the next stepping stone of your learning.
+                Type In your selection from the following options:
+            </p>
+            
+            <ul className="header header__options" >
                 <li>Java</li>
                 <li>Javascript</li>
                 <li>Python</li>
                 <li>React</li>
             </ul>
-        </p>
-        <input 
-            name='search' 
-            onChange={ handleSearch }
-            onKeyPress={ handleEnterKeyPress }
-            placeholder="search for a topic" 
-            type='search' 
-            value={ value } 
-        />
-        <button type='button' onClick={ handleClickSearch }>Search</button>
-        <button type='button' onClick={ handleShowAll } >Show All</button>
+        </article>
+        <article className="header header__search" >
+            <input 
+                className="header header__searchBar"
+                name='search' 
+                onChange={ handleSearch }
+                onKeyPress={ handleEnterKeyPress }
+                placeholder="search for a topic" 
+                type='search' 
+                value={ value } 
+            />
+            <button 
+                className="header header__primary-button" 
+                type='button' 
+                onClick={ handleClickSearch } 
+            >Search
+            </button>
+            <button 
+                className="header header__secondary-button" 
+                type='button' 
+                onClick={ handleShowAll } 
+            >Show All
+            </button>
 
+        </article>
     </header>
 
 );

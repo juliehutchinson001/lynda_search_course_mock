3bb40de25826ad34691d863fb7fa1ede06cfbaa6
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-10-25 11:33:32 -0700

    feat: implement class names to style playlists section

--- my-app/src/Components/Playlist.js
+++ my-app/src/Components/Playlist.js
@@ -4,13 +4,13 @@ const Playlists = () => (
 
     <section id="playlists" className="playlist" >
         <h1 className="playlist playlist__header" >Playlists</h1>
-        <a className="playlist playlist__link" href="#playlists" > Show All </a>
-        <ul className="playlist playlist__list" >
-            <li>
-                <a>Here goes each one of my playlists
-                    <img src="" alt="playlist thumbnail" />
-                    <span>Name of Playlist</span>
-                    <span>Count: </span>
+        <a className="playlist playlist__link--whole" href="#playlists" > Show All </a>
+        <ul className="playlist playlist__group" >
+            <li className="playlist playlist__list" >
+                <a className="playlist playlist__link--single" >Here goes each one of my playlists
+                    <img className="playlist playlist__image" src="" alt="playlist-thumbnail" />
+                    <span className="playlist playlist__header" >Name of Playlist</span>
+                    <span className="playlist playlist__count" >Count: </span>
                 </a>
             </li>        
         </ul>

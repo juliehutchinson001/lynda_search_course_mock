b6fce9760e2a77212b5a25c9e531b04c1aed6bc5
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-10-25 11:14:21 -0700

    feat: update html structure of playlists sections

--- my-app/src/Components/Playlist.js
+++ my-app/src/Components/Playlist.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 const Playlists = () => (
 
-    <aside id="#playlists" className="playlist" >
+    <section id="playlists" className="playlist" >
         <h1 className="playlist playlist__header" >Playlists</h1>
         <a className="playlist playlist__link" href="#playlists" > Show All </a>
-        <section className="playlist playlist__list" >
-            <nav>Here goes each one of my playlists</nav>        
-        </section>
-    </aside>
+        <ul className="playlist playlist__list" >
+            <li>
+                <a>Here goes each one of my playlists
+                    <img src="" alt="playlist thumbnail" />
+                    <span>Name of Playlist</span>
+                    <span>Count: </span>
+                </a>
+            </li>        
+        </ul>
+    </section>
 
 );
 

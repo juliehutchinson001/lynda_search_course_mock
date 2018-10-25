94e533b643efc111d33a6d5d4785280febc5b6fd
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-10-25 11:03:05 -0700

    init: base structure to implement video options 'pop-up'

---
+++ my-app/src/Components/videoOps.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class VideoOptionsContainer extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default VideoOptionsContainer;

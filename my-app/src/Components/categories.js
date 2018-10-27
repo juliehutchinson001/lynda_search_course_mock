16cfcb95b32d9ee3449b848c8571248ca2e03ead
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-10-26 21:08:33 -0700

    feat: form to create new category

--- my-app/src/Components/categories.js
+++ my-app/src/Components/categories.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 const NewCategories = ({ newPlaylist, handleCatDescription, handleCatName, handleFormSubmit }) => (
 
-    
+    <section className={ newPlaylist } >
+        <form onSubmit={ handleFormSubmit } >
+            <label>Playlist name
+                <input name='category-name' type='text' onChange={ handleCatName } required />
+            </label>
+            <label>Description
+                <input name='category-description' type='text' onChange={ handleCatDescription } />
+            </label>
+            <input type='submit' value='Create' />
+        </form>
+    </section>
 
 );
 

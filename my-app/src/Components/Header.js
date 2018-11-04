623edcc516e4b1d7d25e6b4e45b41410b980edc2
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-11-03 21:19:28 -0700

    feat: change name to header.js

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -6,60 +6,59 @@ class HeaderContainer extends Component {
     render() {
 
         return (
-            <Header 
-                value={ this.props.value } 
-                handleClickSearch={ this.props.handleClickSearch }   
-                handleSearch={ this.props.handleSearch }   
-                handleShowAll={ this.props.handleShowAll }   
-                handleEnterKeyPress={ this.props.handleEnterKeyPress }             
+            <Header
+                search={ this.props.search } 
+                handleSearchOnClick={ this.props.handleSearchOnClick }   
+                handleSearchInputChange={ this.props.handleSearchInputChange }   
+                handleShowAllCourses={ this.props.handleShowAllCourses }   
+                handleSearchOnEnter={ this.props.handleSearchOnEnter }             
             />
         );
 
     }
 }
 
-const Header = ({ value, handleClickSearch, handleSearch, handleEnterKeyPress, handleShowAll }) => (
+const Header = ({ search, handleSearchOnClick, handleSearchInputChange, handleSearchOnEnter, handleShowAllCourses }) => (
 
     <header className="header" >
-        <article className="header header__intro" >
-            <h1 className="header header__title" >Lynda Learning</h1>
-            
-            <p className="header header__description" >
+        <section className="header__top" >
+            <h1 className="header__title" >Lynda Learning</h1>
+            <input 
+                className="header__searchBar"
+                name='search'
+                onChange={ handleSearchInputChange }
+                onKeyPress={ handleSearchOnEnter }
+                placeholder="search for a topic"
+                type='search'
+                search={ search }
+            />
+            <button 
+                className="header__primary-button"
+                type='button'
+                onClick={ handleSearchOnClick }
+            >Search
+            </button>
+            <button
+                className="header__secondary-button"
+                type='button' 
+                onClick={ handleShowAllCourses } 
+            >Show All
+            </button>
+        </section>
+        <section className="header__bottom" >
+            <p className="header__description" >
                 Welcome to the next stepping stone of your learning.
                 Type In your selection from the following options:
             </p>
             
-            <ul className="header header__options" >
+            <ul className="header__options" >
                 <li>Java</li>
                 <li>Javascript</li>
                 <li>Python</li>
                 <li>React</li>
             </ul>
-        </article>
-        <article className="header header__search" >
-            <input 
-                className="header header__searchBar"
-                name='search' 
-                onChange={ handleSearch }
-                onKeyPress={ handleEnterKeyPress }
-                placeholder="search for a topic" 
-                type='search' 
-                value={ value } 
-            />
-            <button 
-                className="header header__primary-button" 
-                type='button' 
-                onClick={ handleClickSearch } 
-            >Search
-            </button>
-            <button 
-                className="header header__secondary-button" 
-                type='button' 
-                onClick={ handleShowAll } 
-            >Show All
-            </button>
 
-        </article>
+        </section>
     </header>
 
 );

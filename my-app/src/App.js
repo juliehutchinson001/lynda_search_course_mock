566b2d46c31b0d643c4101e273832ab689d11d32
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-12-03 21:11:31 -0800

    init: first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,37 +2,21 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      courses: [],
-      isInPlaylist: false,
-      playlists: [
-        {
-          name: "My Playlist",
-          count: 0,
-          description: "Default Playlist",
-          videos: {}
-        }
-      ],
-      search: '',
-      validAnswer: true,
-      activeCourse: null,
-      formPlayListFields: {
-        name: "",
-        description: ""
-      }
-    };
-  }
+		this.state = {
 
-  render() {
-    return (
-      <div className="App">
-      </div>
+		};
+	}
 
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+			</div>
+
+		);
+	}
 }
 
 export default App;

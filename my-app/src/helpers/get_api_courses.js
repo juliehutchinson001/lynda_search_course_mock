18bc94944261b6763160b3522f3a866ab83ce50c
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-12-13 04:24:13 -0800

    feat: add functionality to obtain courses from api

--- my-app/src/helpers/get_api_courses.js
+++ my-app/src/helpers/get_api_courses.js
@@ -1,6 +1,14 @@
 const fetchCourses = (category, callback) => {
 
+    const query = category === 'all'
+        ? 'search/all'
+        : `search?categoryName=${category}`;
 
+    const url = `https://mock-course-backend.herokuapp.com/${query}`;
+
+    fetch(url)
+        .then(resp => resp.json())
+        .then(coursesObj => callback(coursesObj.course))
 
 };
 

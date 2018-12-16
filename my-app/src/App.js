aeac7ecc365f0ffc255652d6bb10d9a5a0ecd59e
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-12-16 02:23:09 -0500

    feat: create header component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './components/header';
 
 class App extends Component {
 
@@ -13,7 +14,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-
+				<Header />
 			</div>
 
 		);

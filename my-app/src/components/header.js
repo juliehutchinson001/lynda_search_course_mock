9f2e885705584e23df4df92fa50f7065569c16a9
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-12-22 00:03:49 -0500

    fix: change name of function that handles search

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -2,13 +2,13 @@ import validateSearch from '../helpers/validate_search';
 
 const Header = props => {
     const {
-        onKeyPress
+        handleSearch
     } = props;
 
     return (
         <header>
             <h1>Lynda</h1>
-            <input type='search' onKeyPress={onKeyPress} />
+            <input type='search' onKeyPress={handleSearch} />
             <button type='button'>Search</button>
             <button type='button'>See All</button>
         </header>

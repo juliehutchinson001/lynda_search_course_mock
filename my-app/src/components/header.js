5a842abe8f829da7d36d9cffa82e071bded73969
Julie Hutchinson <julie.gomez001@mymdc.net>
2018-12-28 06:10:57 -0500

    feat: add function to get input search

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -2,13 +2,17 @@ import validateSearch from '../helpers/validate_search';
 
 const Header = props => {
     const {
-        handleSearch
+        handleSearch,
+        getSearchInput,
     } = props;
 
     return (
         <header>
             <h1>Lynda</h1>
-            <input type='search' onKeyPress={handleSearch} />
+            <input
+                type='search'
+                onChange={ getSearchInput }
+                onKeyPress={ handleSearch } />
             <button type='button'>Search</button>
             <button type='button'>See All</button>
         </header>

240fa60a98ec7a6eed198808e5c7f2afc71810bb
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-08-06 20:25:05 -0700

    update imports on the main component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import fetchCourses from './Helpers/get_api_courses';
-import validateSearch from './Helpers/validate_search';
-import HeaderContainer from './Components/header';
-import CourseContainer from './Components/courses';
-import PlaylistSection from './Components/playlist_section';
-import Footer from './Components/footer';
+import React, { Component } from "react";
+import fetchCourses from "./helpers/get_api_courses";
+import validateSearch from "./helpers/validate_search";
+import HeaderContainer from "./components/header";
+import CourseContainer from "./components/courses";
+import PlaylistSection from "./components/playlist_section";
+import Footer from "./components/footer";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
 
@@ -22,7 +21,7 @@ class App extends Component {
           videos: {}
         }
       ],
-      search: '',
+      search: "",
       validAnswer: true,
       activeCourse: null,
       formPlayListFields: {
@@ -33,75 +32,73 @@ class App extends Component {
 
     this.handleSearchOnClick = this.handleSearchOnClick.bind(this);
     this.handleShowAllCourses = this.handleShowAllCourses.bind(this);
-
   }
 
   getCourses() {
     //get the courses from API helper file to update state
-      fetchCourses(this.state.search.toLowerCase(), courses => {
-        this.setState({
-          courses: courses,
-          validAnswer: true
-        })
+    fetchCourses(this.state.search.toLowerCase(), courses => {
+      this.setState({
+        courses: courses,
+        validAnswer: true
       });
+    });
   }
 
   handleSearchOnClick() {
     //validates category of courses is valid after clicking 'search'
     const searchIsValid = validateSearch(this.state.search.toLowerCase());
     searchIsValid ? this.getCourses() : this.showError(true);
-
   }
 
   handleShowAllCourses() {
     //shows all videos
-    this.setState({ courses: [] }, () => fetchCourses('all', allCourses => {
-      this.setState({ courses: allCourses });
-    }));
-
+    this.setState({ courses: [] }, () =>
+      fetchCourses("all", allCourses => {
+        this.setState({ courses: allCourses });
+      })
+    );
   }
 
-  showError(searchNotValid) { searchNotValid && this.setState({ validAnswer: false }); }
+  showError(searchNotValid) {
+    searchNotValid && this.setState({ validAnswer: false });
+  }
 
   handleSearchOnEnter(event) {
-
     //onKeyPress tracks the 'enter' key to call the API request
     const userSearchTerm = this.state.search;
 
     const isValidSearch = validateSearch(userSearchTerm.toLowerCase());
-    const enterKeyWasPressed = event.key === 'Enter' || event.keyCode === 13;
+    const enterKeyWasPressed = event.key === "Enter" || event.keyCode === 13;
 
-    isValidSearch && enterKeyWasPressed ? this.getCourses() : this.showError(enterKeyWasPressed);
-
+    isValidSearch && enterKeyWasPressed
+      ? this.getCourses()
+      : this.showError(enterKeyWasPressed);
   }
 
   //onChange updates the searched term and state
-  handleSearchInputChange(e) { this.setState( { search: e.target.value } ); }
+  handleSearchInputChange(e) {
+    this.setState({ search: e.target.value });
+  }
 
   handleInputName(event) {
-
     const playListName = event.target.value;
     this.setState(oldState => {
-      const newState = {...oldState};
+      const newState = { ...oldState };
       newState.formPlayListFields.name.value = playListName;
       return newState;
     });
-
   }
 
   handleInputDescription(event) {
-
     const playListDescription = event.target.value;
     this.setState(oldState => {
-      const newState = {...oldState};
+      const newState = { ...oldState };
       newState.formPlayListFields.description.value = playListDescription;
       return newState;
     });
-
   }
 
   createPlaylist(event) {
-
     event.preventDefault();
 
     /*
@@ -132,42 +129,37 @@ class App extends Component {
     };
 
     this.setState(oldState => {
-
       return {
         playlists: [...oldState.playlists, newPlaylist]
       };
-
     });
-
   }
-
 
   render() {
     return (
       <div className="App">
         <HeaderContainer
-          handleSearchOnClick={ this.handleSearchOnClick }
-          handleSearchOnEnter={ event => this.handleSearchOnEnter(event)}
-          handleSearchInputChange={ event => this.handleSearchInputChange(event) }
-          handleShowAllCourses={ this.handleShowAllCourses }
-          search={ this.state.search }
+          handleSearchOnClick={this.handleSearchOnClick}
+          handleSearchOnEnter={event => this.handleSearchOnEnter(event)}
+          handleSearchInputChange={event => this.handleSearchInputChange(event)}
+          handleShowAllCourses={this.handleShowAllCourses}
+          search={this.state.search}
         />
         <PlaylistSection
-          playLists={ this.state.playlists }
-          isInPlaylist={ this.state.isInPlaylist }
+          playLists={this.state.playlists}
+          isInPlaylist={this.state.isInPlaylist}
         />
         <CourseContainer
-          isInPlaylist={ this.state.isInPlaylist }
-          courses={ this.state.courses }
-          validAnswer={ this.state.validAnswer }
-          createPlayList={ event => this.createPlaylist(event) }
-          playlists={ this.state.playlists }
-          handleInputName={ event => this.handleInputName(event) }
-          handleInputDescription={ event => this.handleInputDescription(event) }
+          isInPlaylist={this.state.isInPlaylist}
+          courses={this.state.courses}
+          validAnswer={this.state.validAnswer}
+          createPlayList={event => this.createPlaylist(event)}
+          playlists={this.state.playlists}
+          handleInputName={event => this.handleInputName(event)}
+          handleInputDescription={event => this.handleInputDescription(event)}
         />
         <Footer />
       </div>
-
     );
   }
 }

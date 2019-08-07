e3e94bc80443434ed8c8d56f0cc518fa5e72fca6
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-08-06 20:22:58 -0700

    rename components

---
+++ src/components/footer-component.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="footer">
+    By
+    <small className="footer__owner-info">Julie Hutchinson</small>
+  </footer>
+);
+
+export default Footer;

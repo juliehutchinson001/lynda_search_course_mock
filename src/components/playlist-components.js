021c783ce02153878b69013619d1a0c35e35b0b6
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-08-26 17:58:17 -0700

    remove unnecessary markup

--- src/components/playlist-components.js
+++ src/components/playlist-components.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 const PlaylistSection = () => (
-  <section id="playlists" className="playlist">
-    <h1 className="playlist__header">Playlists</h1>
-    <a className="playlist__link--whole" href="#playlists">
-      {" "}
-      Show All{" "}
-    </a>
-    <ul className="playlist__group">
+  <aside id="playlists" className="playlist">
+    <header className="playlist__header-container">
+      <h1 className="playlist__header">Playlists</h1>
+      <a className="playlist__link--whole" href="#playlists">
+        Show All
+      </a>
+    </header>
+    <ul className="playlist__content-container">
       <li className="playlist__list">
         {"TODO: DUMMY HREF, TO FIX LATER"}
         <a href=".playlist.image" className="playlist__link--single">
@@ -21,7 +22,7 @@ const PlaylistSection = () => (
         </a>
       </li>
     </ul>
-  </section>
+  </aside>
 );
 
 export default PlaylistSection;

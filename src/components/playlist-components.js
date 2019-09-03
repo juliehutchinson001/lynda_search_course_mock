394ad45e7c4dce398622913e7e16f61f14a7f636
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-09-03 13:05:02 -0700

    update ui of playlist component

--- src/components/playlist-components.js
+++ src/components/playlist-components.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const PlaylistSection = () => (
+const PlaylistSection = ({ playLists, isInPlaylist }) => (
   <aside id="playlists" className="playlist">
     <header className="playlist__header-container">
       <h1 className="playlist__header">Playlists</h1>
@@ -8,21 +8,33 @@ const PlaylistSection = () => (
         Show All
       </a>
     </header>
-    <ul className="playlist__content-container">
-      <li className="playlist__list">
-        {"TODO: DUMMY HREF, TO FIX LATER"}
-        <a href=".playlist.image" className="playlist__link--single">
-          <img
-            className="playlist__image"
-            src="https://via.placeholder.com/110x50"
-            alt="playlist-thumbnail"
-          />
-          <span className="playlist__header">Name of Playlist</span>
-          <span className="playlist__count">Count: </span>
-        </a>
-      </li>
-    </ul>
+    <section className="playlist__legend--middle-section">
+      <p className="playlist__legend-text">
+        Add courses by clicking the &#43; to save and watch later.
+      </p>
+    </section>
+    <PlaylistComponent playLists={playLists} isInPlaylist={isInPlaylist} />
   </aside>
 );
+
+const PlaylistComponent = ({ playLists, isInPlaylist }) => {
+  const allPlaylists = playLists.map(({ name, count }) => (
+    <li className="playlist__list" key={name}>
+      <a href=".playlist.image" className="playlist__link--single">
+        <img
+          className="playlist__image"
+          src="https://via.placeholder.com/110x50"
+          alt="playlist-thumbnail"
+        />
+        <section className="playlist__information">
+          <span className="playlist__header">{name}</span>
+          <span className="playlist__count">Count: {count}</span>
+        </section>
+      </a>
+    </li>
+  ));
+
+  return <ul className="playlist__content-container">{allPlaylists}</ul>;
+};
 
 export default PlaylistSection;
